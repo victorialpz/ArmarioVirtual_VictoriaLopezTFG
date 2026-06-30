59d6746828623fa319e795472295046131a81b08
victorialpzzz <victorialopez26@icloud.com>
2026-06-30 23:51:18 +0100

    chore: eliminar config.example.ts innecesario

--- constants/config.example.ts
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_URL      = 'YOUR_SUPABASE_URL';
-export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
-export const OPENWEATHER_API_KEY = 'YOUR_OPENWEATHER_API_KEY';
-export const API_BASE = 'http://localhost:8000';
